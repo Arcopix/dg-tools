d520be83a58dffc88d544181dc132f9077793d71
Stefan Lekov <stefan.lekov@devhex.org>
2020-06-02 02:31:25 +0300

    Implemented navigate in fleets using ARROW keys
    Implemented full navigation through fleets using ARROW keys

--- tools.js
+++ tools.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name       DG utilities
+// @name       DG utilities
 // @namespace  devhex
-// @version    0.3.0002
+// @version    0.3.0004
 // @description  various minor improvements of DG interface
 // @match      https://beta.darkgalaxy.com/
 // @match      https://beta.darkgalaxy.com/*
-// @copyright  2020, Stefan Lekov / Arcopix / Devhex Ltd
+// @copyright  2020, Stefan Lekov / Arcopix / Devhex Ltd
 // ==/UserScript==
 
 /* TODO:
@@ -63,7 +63,7 @@ for (var i=0; i<elems.length; i++) {
     if (e.className=="allianceName"&&nap_ally.includes(e.innerText.trim())) {
         e.style = custom_style;
         var p = e.parentElement;
-for (var j=0; j<p.children.length; j++) {
+        for (var j=0; j<p.children.length; j++) {
             if (p.children[j].className=="playerName") {
                 p.style = custom_style;
             }
@@ -100,7 +100,6 @@ for (i=0; i<elems.length; i++) {
         }
         if (p.children[j].className=="left width25") {
             left = p.children[j].innerText;
-
             if (left>0) {
                 add_confirm = 1;
             }
@@ -139,6 +138,35 @@ if (document.querySelector('#planetHeader .planetName a:nth-of-type(1)')) {
         }
     });
 }
+
+/* Navigate through fleets using ARROW keys */
+if (document.querySelector('.nextPrevFleet, .left')) {
+    /* If we have only RIGHT fleet, meaning we are only at first one, activate RIGHT ARROWO ONLY */
+    if (document.querySelector('.nextPrevFleet').innerText === '»') {
+        document.addEventListener("keydown", e => {
+            if (e.which === 39) {
+                document.querySelectorAll('.nextPrevFleet a:nth-of-type(1)')[0].click();
+            }
+        });
+    } else if (typeof (document.querySelectorAll('.nextPrevFleet')[0]) !== 'undefined' &&
+               typeof (document.querySelectorAll('.nextPrevFleet')[1]) !== 'undefined') {
+        document.addEventListener("keydown", e => {
+            if (e.which === 37) {
+                document.querySelectorAll('.nextPrevFleet a:nth-of-type(1)')[0].click();
+            }
+            if (e.which === 39) {
+                document.querySelectorAll('.nextPrevFleet a:nth-of-type(1)')[1].click();
+            }
+        });
+    } else { /* In case of '«' */
+        document.addEventListener("keydown", e => {
+            if (e.which === 37) {
+                document.querySelectorAll('.nextPrevFleet a:nth-of-type(1)')[0].click();
+            }
+        });
+    }
+}
+
 
 /* No longer showing "FREE" population, show total population,
   Original concept by Mordread
@@ -195,11 +223,10 @@ for (i=0; i<coords.length; i++)
     c.innerHTML = '<a href="/navigation/' + p[0] + '/' + p[1] + '/">' + c.innerHTML + '</a>';
 }
 
-
 if (0) {
 
 /* Under development / testing on medusa */
-	
+
 /* Fix sorting of radarts */
 var radars, radar, fleetRow, fleetCount;
 if (location.href.includes('/radar/')) {
@@ -224,6 +251,7 @@ if (location.href.includes('/radar/')) {
     }
     //alert("we are the radar mate");
 }
-}
 
+}
+    
 /* End of script */
