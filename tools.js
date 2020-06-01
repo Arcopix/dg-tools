9a29a482a03993f56f6422d51d9a5875c20e5a57
Stefan Lekov <stefan.lekov@devhex.org>
2020-06-01 22:18:41 +0300

    Add files via upload
    Initial import of the toolkit

---
+++ tools.js
@@ -0,0 +1,230 @@
+// ==UserScript==
+// @name       DG S0L color coding
+// @namespace  devhex
+// @version    0.3.0001
+// @description  various minor improvements of DG interface
+// @match      https://beta.darkgalaxy.com/
+// @match      https://beta.darkgalaxy.com/*
+// @copyright  2013, Stefan Lekov / Arcopix / Devhex Ltd
+// ==/UserScript==
+
+/* TODO:
+integrate/refactor scripts 1, 2 and 4 from Mord */
+
+/* function to decode URI params */
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+/* Go to mail module and set recepient */
+function configureMail(player)
+{
+    window.location.href = "https://beta.darkgalaxy.com/mail/?to=" + player;
+}
+
+/* Formatting numbers */
+function formatNumber(num)
+{
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+/* common function in css to change css style */
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+/* Add custom styles */
+addGlobalStyle('.sol { COLOR: #027780; }');
+
+var nap_ally = [ "[S0L]", "[RoF]" ];
+
+var custom_style = "COLOR: #FFD54F;"; // CHANGE HERE
+var elems = document.getElementsByTagName("div");
+var deb=0;
+var player="";
+for (var i=0; i<elems.length; i++) {
+    var e = elems[i];
+    if (e.className=="allianceName"&&nap_ally.includes(e.innerText.trim())) {
+        e.style = custom_style;
+        var p = e.parentElement;
+        for (var j=0; j<p.children.length; j++) {
+            if (p.children[j].className=="playerName") {
+                p.style = custom_style;
+            }
+        }
+    }
+}
+
+/* Add onclick to player names through the interface to forward to mail module */
+elems = document.getElementsByClassName("playerName");
+for (i=0; i<elems.length; i++) {
+    e = elems[i];
+    e.addEventListener('click', function() { configureMail(this.innerText.trim()) }, false);
+}
+
+/* If in mail module and to is set -> set the recepient */
+if (window.location.pathname=="/mail/" && window.location.search!=="") {
+    var query = getQueryParams(document.location.search);
+    if (typeof query.to != 'undefined' && query.to != "") {
+        document.getElementsByName('to')[0].value = query.to;
+    }
+}
+var name, left;
+
+/* Add confirmation on canceling buildings */
+elems = document.getElementsByClassName("queueRemoveButton");
+for (i=0; i<elems.length; i++) {
+    var add_confirm = 0;
+    e = elems[i];
+    p = e.parentElement.parentElement;
+
+    for (j=0; j<p.children.length; j++) {
+        //alert(p.children[j].className);
+        if (p.children[j].className=="left name") {
+            name = p.children[j].innerText;
+        }
+        if (p.children[j].className=="left width25") {
+            left = p.children[j].innerText;
+
+            if (left>0) {
+                add_confirm = 1;
+            }
+        }
+    }
+
+    if (add_confirm) {
+        e.confirmString = "Are you sure you want to cancel " + name + "?";
+        e.addEventListener('click', function(evt) { if (confirm(evt.currentTarget.confirmString)===false) evt.preventDefault(); });
+    }
+}
+
+
+/* Script by Mordread -> use ARROW keys to navigate in navigation
+   fix by Arcopix - removed anonymous function, since it was useless */
+if (document.querySelector(".navigation.left")) {
+    document.addEventListener("keydown", (e) => {
+        if (e.which === 37) {
+            document.querySelector(".navigation.left").click();
+        }
+        if (e.which === 39) {
+            document.querySelector(".navigation.right").click();
+        }
+    });
+}
+
+/* Script by Mordread -> use ARROW keys to navigate in planet details
+   fix by Arcopix - removed anonymous function, since it was useless */
+if (document.querySelector('#planetHeader .planetName a:nth-of-type(1)')) {
+    document.addEventListener("keydown", e => {
+        if (e.which === 37) {
+            document.querySelector('#planetHeader .planetName a:nth-of-type(1)').click();
+        }
+        if (e.which === 39) {
+            document.querySelector('#planetHeader .planetName a:nth-of-type(2)').click();
+        }
+    });
+}
+
+/* No longer showing "FREE" population, show total population,
+  Original concept by Mordread
+  Refactored by Arcopix
+*/
+var numberWithCommas;
+var line;
+var availablePopulation, occupiedPopulation;
+var planets;
+if (location.href.includes('/planet/')) {
+    numberWithCommas = (x) => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    line = document.getElementsByClassName('resource')[3];
+    availablePopulation = line.getElementsByTagName('span')[0].innerText.split(' / ')[0];
+    occupiedPopulation = line.getElementsByTagName('span')[2].innerText.replace('(', '').replace(' occupied)', '')
+
+    availablePopulation = parseInt(availablePopulation.replace(',', ''));
+    occupiedPopulation = parseInt(occupiedPopulation.replace(',', ''));
+    line.getElementsByTagName('span')[0].innerText = formatNumber(availablePopulation+occupiedPopulation) + ' / ' + line.getElementsByTagName('span')[0].innerText.split(' / ')[1]
+}
+
+if (window.location.pathname=="/planets/") {
+    planets = document.getElementsByClassName('locationWrapper');
+    for (i=0; i<planets.length; i++) {
+        line = planets[i].getElementsByClassName('resource')[3];
+
+        availablePopulation = line.getElementsByTagName('span')[0].innerText.split(' / ')[0];
+        occupiedPopulation = line.getElementsByTagName('span')[1].innerText.replace('(', '').replace(' occupied)', '');
+
+        availablePopulation = parseInt(availablePopulation.replace(',', ''));
+        occupiedPopulation = parseInt(occupiedPopulation.replace(',', ''));
+
+        line.getElementsByTagName('span')[0].innerText = formatNumber(availablePopulation+occupiedPopulation);
+    }
+}
+
+/* Coordinates as links */
+var coords;
+coords = document.getElementsByClassName('coords')
+for (i=0; i<coords.length; i++)
+{
+    let c = coords[i];
+//    if (c.tagName!='div') {
+//        continue;
+//    }
+
+    c = c.getElementsByTagName('span')[0];
+
+    /* Don't bother with home planets */
+    if (c.innerText=='0.0.0') {
+        continue;
+    }
+    let p = c.innerText.split('.');
+
+    c.innerHTML = '<a href="/navigation/' + p[0] + '/' + p[1] + '/">' + c.innerHTML + '</a>';
+}
+
+
+if (0) {
+
+/* Under development / testing on medusa */
+	
+/* Fix sorting of radarts */
+var radars, radar, fleetRow, fleetCount;
+if (location.href.includes('/radar/')) {
+    radars = document.getElementsByClassName('opacDarkBackground');
+    for (i=0; i<radars.length; i++) {
+        if (radars[i].className.includes('paddingMid')) {
+            continue;
+        }
+        radar = radars[i];
+        fleetRow = radar.getElementsByClassName('entry');
+
+        for (j=fleetRow.length-1; j>=0; j--) {
+            ;//fleetRow[j].parentNode.removeChild(fleetRow[j]);
+        }
+/*
+        alert(e.innerHTML);
+        e.detach().sort(function (one, two) {
+            let a = parseInt(one.getElementsByClass('turns')[0].innerText)
+            let b = parseInt(two.getElementsByClass('turns')[0].innerText)
+            return( a <b> b ? 1 : 0 );
+        });*/
+    }
+    //alert("we are the radar mate");
+}
+}
+
+/* End of script */
